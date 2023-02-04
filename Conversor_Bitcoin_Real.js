b32891803e8780b5781897b4e8a301e69d5bb3a1
Edivânia NS <edneres7@gmail.com>
2023-02-04 18:23:27 -0300

    Desafio 1 - Aula 1

---
+++ Conversor_Bitcoin_Real.js
@@ -0,0 +1,11 @@
+//////////////////
+//   EDIVÂNIA   //
+//     UFPI     //
+//   04/02/23   //
+//////////////////
+
+var cotacao_bitcoin = 120620.12  // cotação em 04/02/23
+var valor_bitcoin = 0.25
+var valor_real = cotacao_bitcoin * valor_bitcoin
+
+alert("O valor convertido é de R$" + valor_real.toFixed(2))
