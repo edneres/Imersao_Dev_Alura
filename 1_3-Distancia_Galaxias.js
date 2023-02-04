d5969af2a51a4d8bb3d66795d2ab8fa8e474848e
Edivânia NS <edneres7@gmail.com>
2023-02-04 18:40:23 -0300

    Desafio 3

---
+++ 1_3-Distancia_Galaxias.js
@@ -0,0 +1,10 @@
+//////////////////
+//   EDIVÂNIA   //
+//     UFPI     //
+//   04/02/23   //
+//////////////////
+
+var al_metros = 9.43 * (10**15)  // al = anos(s) luz
+var distancia_al = 2.5 * (10**6)
+
+alert("A distância entre a Via Láctea e Andômeda é de " + (al_metros * distancia_al) +  "m!")
