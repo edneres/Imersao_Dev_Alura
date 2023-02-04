f53629b71bc7aecdc15cfd06181cb0012e8448f4
Edivânia NS <edneres7@gmail.com>
2023-02-04 19:36:18 -0300

    Desafio 1

---
+++ 2_1-Cont_Chutes.js
@@ -0,0 +1,24 @@
+//////////////////
+//   EDIVÂNIA   //
+//     UFPI     //
+//   04/02/23   //
+//////////////////
+
+var num_secreto = parseInt(Math.random() * 1001 ) // analise como inteiro...
+var cont = 0
+var chute = prompt ("Chute um número entre 0 - 1000: ")
+
+while(chute != num_secreto)
+ {
+    cont++
+   
+    if (chute > num_secreto)
+      alert("Você errou! O número secreto é menor que " + chute + ".\n" + "Chute(s): " + cont)
+
+    else
+      alert("Você errou! O número secreto é maior que " + chute + ".\n" + "Chute(s): " + cont)
+   
+    var chute = prompt ("Chute um número entre 1 - 1000: ")
+  }
+
+alert("Você acertou com " + (cont++) + " chutes!")
