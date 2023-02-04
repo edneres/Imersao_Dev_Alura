ddbbd65bc571a51a459fb3276a90864a99b1486e
Edivânia NS <edneres7@gmail.com>
2023-02-04 18:28:02 -0300

    Desafio 2

---
+++ 1.2 - Conversor_com_Nome.js
@@ -0,0 +1,12 @@
+//////////////////
+//   EDIVÂNIA   //
+//     UFPI     //
+//   04/02/23   //
+//////////////////
+
+var cotacao_bitcoin = 120620.12  // cotação em 04/02/23
+var valor_bitcoin = 0.25
+var valor_real = cotacao_bitcoin * valor_bitcoin
+var nome = "Edivânia"
+
+alert("O valor convertido, " + nome +  ", é de R$" + valor_real.toFixed(2))
