2caf948a0f6a5d4b4252f43dbeb54f2d7632ccc0
Edivânia NS <edneres7@gmail.com>
2023-02-04 18:16:55 -0300

    Create Aula1.js

---
+++ Aula1.js
@@ -0,0 +1,11 @@
+//////////////////
+//   EDIVÂNIA   //
+//     UFPI     //
+//   04/02/23   //
+//////////////////
+
+var cotacao_dolar = 5.15  // cotação em 04/02/23
+var valor_dolar = 30
+var valor_real = cotacao_dolar * valor_dolar
+
+alert("O valor convertido é de R$" + valor_real.toFixed(2))
